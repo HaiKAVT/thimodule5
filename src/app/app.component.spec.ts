f25bc1761f6d7c4652f49681a7bef9a0a3c7e500
HaiKAVT <haivtka@gmail.com>
2021-09-30 12:08:26 +0700

    metmoi

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'examAngular'`, () => {
+  it(`should have as title 'Thi-Module5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('examAngular');
+    expect(app.title).toEqual('Thi-Module5');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('examAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Thi-Module5 app is running!');
   });
 });
